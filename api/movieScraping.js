06c0b9932d59c33271f8ca4aae58c238418aa20f
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-02-27 02:24:24 -0600

    update

--- api/movieScraping.js
+++ api/movieScraping.js
@@ -3,7 +3,9 @@ const puppeteer = require('puppeteer');
 
 module.exports = async (req, res) =>{
         try {
-          const browser = await puppeteer.launch();
+          const browser = await puppeteer.launch({
+            executablePath: '/Users/horuy/.cache/puppeteer/chrome/mac_arm-122.0.6261.69/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing'
+          });
           const page = await browser.newPage();
           await page.goto('https://letterboxd.com/stoopidass/list/fav/');
       
