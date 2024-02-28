03be14f55db80544688e470d8ebd13fb37cc4c67
horuy <69568816+horuyc7@users.noreply.github.com>
2024-02-28 14:23:13 -0600

    update dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,16 @@
 import React from "react";
-import TasksCard from "./component/TasksCard";
 import "./styles.css";
 
 export default function Dashboard() {
   return (
     <div>
       <h2 className="mt-3">Dashboard</h2>
+
+      https://github.com/horuyc7/ryanchiv
+      
       <div className="row mt-5">
         <div className="col-12">
-          <TasksCard />
+          
         </div>
       </div>
     </div>
