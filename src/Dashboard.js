ca2a844f0d1c6dcfcb9d1788dc49cef1d648176d
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-03-01 03:50:54 -0600

    Update

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,18 +1,20 @@
 import React from "react";
 import FeaturesCard from "./component/FeaturesCard";
+import Skills from "./component/Skills";
 import "./styles.css";
 
 export default function Dashboard() {
   return (
-    <div>
-      <h2 className="mt-3">Dashboard</h2>
+    <div className="dashboard">
+      <h2 className="dashboard-title">Dashboard</h2>
       
       <div className="row mt-5">
           
         <div className="col-12">
           <FeaturesCard/>
-          
+          <Skills/>
         </div>
+        
       </div>
     </div>
   );
