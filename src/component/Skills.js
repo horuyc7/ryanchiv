5f89cf10285f4b9feffd43f8631a790a518195b3
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-03-09 02:57:37 -0600

    Update Skills.js

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -123,94 +123,82 @@ export default function Skills() {
         </div>
       </div>
 
-
-      <div className="skills-header2">Tools
-      
-        <div className="skills-body">
-            <div className="skills">
-
-
-                 <div className="skill" style={{width:"100%"}}>
-                            <div className="skill-bar">
-                                <div className="skill-name">VS
-                                    <span className="skill-line" style={{width: "14.8333%"}}></span>
-                                </div>
-                                <div className="skill-loader" style={{width: "85%"}}></div>
-                            </div>
-                        
+      <div className="skills-header2"> Tools
+    <div className="skills-body">
+        <div className="skills">
+            <div className="skill" style={{width:"67%"}}>
+                <div className="skill-bar">
+                    <div className="skill-name">VS
+                        <span className="skill-line" style={{width: "14.8333%"}}></span>
+                    </div>
+                    <div className="skill-loader" style={{width: "85%"}}></div>
                 </div>
+            </div>
 
-                <div className="skill" style={{width:"100%"}}>
-                            <div className="skill-bar">
-                                <div className="skill-name">IntelliJ
-                                    <span className="skill-line" style={{width: "14.8333%"}}></span>
-                                </div>
-                                <div className="skill-loader" style={{width: "80%"}}></div>
-                            </div>
-                        
+            <div className="skill" style={{width:"35%"}}>
+                <div className="skill-bar">
+                    <div className="skill-name">Intellij
+                        <span className="skill-line" style={{width: "14.8333%"}}></span>
+                    </div>
+                    <div className="skill-loader" style= {{width: "75%"}}></div>
                 </div>
+            </div>
 
-                <div className="skill" style={{width:"100%"}}>
-                                <div className="skill-bar">
-                                    <div className="skill-name">GNS3
-                                        <span className="skill-line" style={{width: "14.8333%"}}></span>
-                                    </div>
-                                    <div className="skill-loader" style={{width: "60%"}}></div>
-                                </div>
-                            
+            <div className="skill" style={{width:"55%"}}>
+                <div className="skill-bar">
+                    <div className="skill-name">GNS3
+                        <span className="skill-line" style={{width: "14.8333%"}}></span>
+                    </div>
+                    <div className="skill-loader" style={{width: "65%"}}></div>
                 </div>
+            </div>
 
-                <div className="skill" style={{width:"100%"}}>
-                            <div className="skill-bar">
-                                <div className="skill-name">Postman
-                                    <span className="skill-line" style={{width: "14.8333%"}}></span>
-                                </div>
-                                <div className="skill-loader" style={{width: "65%"}}></div>
-                            </div>
-                        
+            <div className="skill" style={{width:"50%"}}>
+                <div className="skill-bar">
+                    <div className="skill-name">Postman
+                        <span className="skill-line" style={{width: "14.8333%"}}></span>
+                    </div>
+                    <div className="skill-loader" style={{width: "70%"}}></div>
                 </div>
+            </div>
 
-
-                <div className="skill" style={{width:"100%"}}>
-                            <div className="skill-bar">
-                                <div className="skill-name">Wireshark
-                                    <span className="skill-line" style={{width: "14.8333%"}}></span>
-                                </div>
-                                <div className="skill-loader" style={{width: "60%"}}></div>
-                            </div>
-                        
+            <div className="skill" style={{width:"45%"}}>
+                <div className="skill-bar">
+                    <div className="skill-name">Wireshark
+                        <span className="skill-line" style={{width: "14.8333%"}}></span>
+                    </div>
+                    <div className="skill-loader" style={{width: "65%"}}></div>
                 </div>
+            </div>
 
-                <div className="skill" style={{width:"100%"}}>
-                            <div className="skill-bar">
-                                <div className="skill-name">Eclipse
-                                    <span className="skill-line" style={{width: "14.8333%"}}></span>
-                                </div>
-                                <div className="skill-loader" style={{width: "90%"}}></div>
-                            </div>
-                        
+            <div className="skill" style={{width:"30%"}}>
+                <div className="skill-bar">
+                    <div className="skill-name">Eclipse
+                        <span className="skill-line" style={{width: "14.8333%"}}></span>
+                    </div>
+                    <div className="skill-loader" style={{width: "75%"}}></div>
                 </div>
+            </div>
 
-            
-
-                <div className="skill" style={{width:"100%"}}>
-                            <div className="skill-bar">
-                                <div className="skill-name">Cisco Routers
-                                    <span className="skill-line" style={{width: "14.8333%"}}></span>
-                                </div>
-                                <div className="skill-loader" style={{width: "60%"}}></div>
-                            </div>
-                        
+            <div className="skill" style={{width:"55%"}}>
+                <div className="skill-bar">
+                    <div className="skill-name">VMs
+                        <span className="skill-line" style={{width: "14.8333%"}}></span>
+                    </div>
+                    <div className="skill-loader" style={{width: "70%"}}></div>
                 </div>
-
-            
-
+            </div>
+            <div className="skill" style={{width:"75%"}}>
+                <div className="skill-bar">
+                    <div className="skill-name">Cisco Routers
+                        <span className="skill-line" style={{width: "14.8333%"}}></span>
+                    </div>
+                    <div className="skill-loader" style={{width: "60%"}}></div>
+                </div>
             </div>
         </div>
-      </div>
-
-
-
+    </div>
+</div>
 
 
     </div>
