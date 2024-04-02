e36eb9a76980ac38497b831cf5c79a32deb6a603
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-04-02 02:08:10 -0500

    Update Restaurants.js

--- src/component/Restaurants.js
+++ src/component/Restaurants.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
 
 import '../css/Restaurants.css';
 
 export default function Restaurants(){
 
+    const [loading, setLoading] = useState(true);
+    const [showIframe, setShowIframe] = useState(false);
+
+
     return(
         <div className="restaurants">
-            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1D3eu-3oCDeBTG-DDEE-2YqJwQp__2RY&ehbc=2E312F&noprof=1" 
-            width="100%" height="600px"></iframe>
+
+        
+                <div className="restaurants-container">
+                    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1D3eu-3oCDeBTG-DDEE-2YqJwQp__2RY&ehbc=2E312F&noprof=1" 
+                        width="100%" height="600px"></iframe>
+                </div>
+            
         </div>
     );
-
 }
