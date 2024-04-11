aff42814ba2600bbd58ef485a772af5967a53d8a
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-04-11 02:13:46 -0500

    Update Skills.js

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -178,6 +178,41 @@ export default function Skills() {
             </div>
         </div>
 
+
+
+        <div className="skills__header2">Languages
+            <div className="skills__body">
+
+                <div className="skill-container" style={{width:"100%"}}>
+                    <div className="skill-bar">
+                        <div className="skill-name">English
+                            <span className="skill-line" style={{width: "14.8333%"}}></span>
+                        </div>
+                        <div className="skill-loader" style={{width: "100%"}}></div>
+                    </div> 
+                </div>
+
+                <div className="skill-container" style={{width:"90%"}}>
+                    <div className="skill-bar">
+                        <div className="skill-name">Japanese
+                            <span className="skill-line" style={{width: "14.8333%"}}></span>
+                        </div>
+                        <div className="skill-loader" style={{width: "70%"}}></div>
+                    </div>
+                </div>
+
+                <div className="skill-container" style={{width:"95%"}}>
+                    <div className="skill-bar">
+                        <div className="skill-name">Khmer
+                            <span className="skill-line" style={{width: "14.8333%"}}></span>
+                        </div>
+                        <div className="skill-loader" style={{width: "60%"}}></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
     </div>
   );
 }
