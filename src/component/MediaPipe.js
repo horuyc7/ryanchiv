e8c0992f783695b1fa30cd994d19cb61e64eaff1
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-05-28 15:54:32 -0500

    Update MediaPipe.js

--- src/component/MediaPipe.js
+++ src/component/MediaPipe.js
@@ -1,137 +1,171 @@
 import React, { useState, useEffect, useRef } from 'react';
-
 import {
     ObjectDetector,
     FilesetResolver,
-  } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
-
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
 import Loading from './Loading';
-  
-
-  
 import '../css/MediaPipe.css';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Checkbox } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#d6550e',
+      
+    },
+
+    secondary: {
+      main: '#8be4a9',
+      
+    },
 
 
+    mode: 'dark',
+
+    
+  },
+
+
+});
 
 const MediaPipe = ({ imageUrl }) => {
-  const [objectDetector, setObjectDetector] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [objects, setObjects] = useState([]);
-  const fileInputRef = useRef(null);
-  const [image, setImage] = useState(null);
+    const [objectDetector, setObjectDetector] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [objects, setObjects] = useState([]);
+    const fileInputRef = useRef(null);
+    const [image, setImage] = useState(null);
+    const [selectedObjects, setSelectedObjects] = useState([]);
 
-  useEffect(() => {
-    let runningMode = "IMAGE";
-    setLoading(true);
+    useEffect(() => {
+        let runningMode = "IMAGE";
+        setLoading(true);
 
-    const initializeObjectDetector = async () => {
-        const vision = await FilesetResolver.forVisionTasks(
-            "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
-        );
-        const objectDetector = await ObjectDetector.createFromModelPath(vision,
-            "https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite"
-        );
-          setObjectDetector(objectDetector);
-          setLoading(false);
+        const initializeObjectDetector = async () => {
+            const vision = await FilesetResolver.forVisionTasks(
+                "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
+            );
+            const objectDetector = await ObjectDetector.createFromModelPath(vision,
+                "https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite"
+            );
+            setObjectDetector(objectDetector);
+            setLoading(false);
+        };
+
+        initializeObjectDetector();
+    }, []);
+
+    const classifyImage = async (img) => {
+        if (objectDetector) {
+            const result = objectDetector.detect(img);
+            setObjects(result.detections);
+            setLoading(false);
+        }
     };
-    
-    initializeObjectDetector();
-  }, []);
 
-  const classifyImage = async (img) => {
-    if (objectDetector) {
-      const result = objectDetector.detect(img);
-      setObjects(result.detections);
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
 
-      setLoading(false);
-    }
-  };
-
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = async () => {
-      const img = new Image();
-      img.src = reader.result;
-      img.onload = () => {
-        setImage(img);
-        classifyImage(img);
-      };
+        reader.onload = async () => {
+            const img = new Image();
+            img.src = reader.result;
+            img.onload = () => {
+                setImage(img);
+                classifyImage(img);
+                setSelectedObjects([]);
+            };
+        };
+        reader.readAsDataURL(file);
     };
-    reader.readAsDataURL(file);
-  };
 
-  const triggerImageUpload = () => {
-    fileInputRef.current.click();
-  };
+    const triggerImageUpload = () => {
+        fileInputRef.current.click();
+    };
 
-  return (
-    <div className="object-detection-container">
-      {loading ? (
-        <div className='loading'>
-          <Loading/>
+    const handleObjectSelect = (object) => {
+        const index = selectedObjects.findIndex(obj => obj === object);
+        if (index !== -1) {
+            setSelectedObjects(selectedObjects.filter(obj => obj !== object));
+        } else {
+            setSelectedObjects([...selectedObjects, object]);
+        }
+    };
+
+    const handleCheckboxChange = (object) => {
+        const index = selectedObjects.findIndex(obj => obj === object);
+        if (index !== -1) {
+            setSelectedObjects(selectedObjects.filter(obj => obj !== object));
+        } else {
+            setSelectedObjects([...selectedObjects, object]);
+        }
+    };
+
+    return (
+        <div className="object-detection-container">
+            {loading ? (
+                <div className='loading'>
+                    <Loading />
+                </div>
+            ) : (
+                <div className="image-container">
+                    <h2 className='title'>Object Detection</h2>
+                    <div className='input'>
+                        <input ref={fileInputRef} type="file" accept="image/*" onChange={handleImageUpload} style={{ display: 'none', marginBottom: '20px' }} />
+                        <button className='upload-button' onClick={triggerImageUpload}>Upload Image</button>
+                    </div>
+                    <div className="image-wrapper">
+                        {image && <img src={image.src} alt="Uploaded" />}
+                        {selectedObjects.map((object, index) => (
+                            <div
+                                key={index}
+                                className="highlighter"
+                                style={{
+                                    left: `${(object.boundingBox.originX / image.width) * 100}%`,
+                                    top: `${(object.boundingBox.originY / image.height) * 100}%`,
+                                    width: `${(object.boundingBox.width / image.width) * 100}%`,
+                                    height: `${(object.boundingBox.height / image.height) * 99.6}%`
+                                }}>
+                            </div>
+                        ))}
+                    </div>
+
+                    {objects && objects.length > 0 && (
+                    <TableContainer component={Paper}>
+                        <Table sx={{backgroundColor: Theme.palette.primary.main, color: Theme.palette.secondary.main}}>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell sx={{ color: '#8be4a9' }}></TableCell>
+                                    <TableCell sx={{ color: '#8be4a9' }}>Object Name</TableCell>
+                                    <TableCell sx={{ color: '#8be4a9' }}>Confidence</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {objects.slice(0,10).map((object, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell>
+                                            <Checkbox sx={{ color: 'white' }}
+                                                checked={selectedObjects.includes(object)}
+                                                onChange={() => handleCheckboxChange(object)}
+                                            />
+                                        </TableCell>
+                                        <TableCell sx={{ color: 'white' }} onClick={() => handleObjectSelect(object)}>
+                                            {object.categories[0].categoryName}
+                                        </TableCell>
+                                        <TableCell sx={{ color: 'white' }} onClick={() => handleObjectSelect(object)}>
+                                            {Math.round(parseFloat(object.categories[0].score) * 100)}%
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    )}
+                </div>
+            )}
         </div>
-      ) : (
-        
-        <div className="image-container">
-            
-
-            <h2 className='title'>Object Detection</h2>
-
-            <div className='input'>
-              <input ref={fileInputRef} type="file" accept="image/*" onChange={handleImageUpload} style={{ display: 'none', marginBottom: '20xp'}} />
-              <button className='upload-button' onClick={triggerImageUpload}>Upload Image</button>
-            </div>
-
-
-            <div className="image-wrapper">
-              
-
-              {image && <img src={image.src} alt="Uploaded" />}
-
-
-              {objects.filter(detection => parseFloat(detection.categories[0].score) > 0.2).length === 0 &&image && <p>No object detected</p>}
-            
-            
-              {objects.filter(detection => parseFloat(detection.categories[0].score) > 0.2).slice(0,10).map((detection, index) => (
-                    <React.Fragment key={index}>
-                        <div
-                            className="highlighter"
-                            style={{
-                                left: `${(detection.boundingBox.originX / image.width) * 100}%`,
-                                top: `${(detection.boundingBox.originY / image.height) * 100}%`,
-                                width: `${(detection.boundingBox.width / image.width) * 100}%`,
-                                height: `${(detection.boundingBox.height / image.height) * 99.6}%`
-                            }}>
-
-                            <p className="info">
-                                {detection.categories[0].categoryName}{" "}{index}
-                            </p>
-                        </div>  
-                    </React.Fragment>
-
-                    
-                ))}
-            </div>
-
-            
-            <div className='confidence'>
-                {objects.filter(detection => parseFloat(detection.categories[0].score) > 0.2).slice(0, 10).map((detection, index) => (
-                      <div>
-                          <p>
-                              {detection.categories[0].categoryName} {index}: {Math.round(parseFloat(detection.categories[0].score) * 100)}% confidence.
-                          </p>
-                      </div>  
-                  ))}
-            </div>
-
-          
-
-          </div>
-        )}
-    </div>
-  );
+    );
 };
 
 export default MediaPipe;
+
