ce782077cc432af43193e37522cd7ed8c057f781
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-06-10 16:09:10 -0500

    Update Travel.js

--- src/component/Travel.js
+++ src/component/Travel.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 
 const Travel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   return (
+
+    
     <div style={{ position: 'relative', width: '100vw', height: '90vh', cursor: 'pointer' }} onClick={nextImage}>
       <img 
         src={images[currentIndex]} 
