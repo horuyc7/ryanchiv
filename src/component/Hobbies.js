73b1d2104e413db288b774b5d28d546f65411ff5
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-06-10 16:24:40 -0500

    Update Hobbies.js

--- src/component/Hobbies.js
+++ src/component/Hobbies.js
@@ -4,24 +4,24 @@ import "../css/Hobbies.css"
 export default function Hobbies() {
   return (
     <div className="hobbiescard">
-        <div className="featurescard__header">Interests/Hobbies</div>
+        <div className="hobbiescard__header">Interests/Hobbies</div>
 
-        <div className="featurescard__body">
-            <ul className="featurescard_items">
+        <div className="hobbiescard__body">
+            <ul className="hobbiescard__items">
 
-                <li className="featurescard__item">Pool (Snooker, 9/10 ball, Straight)</li>
-                <li className="featurescard__item">Manga</li>
-                <li className="featurescard__item">Movies</li>
-                <li className="featurescard__item">Soccer</li>
-                <li className="featurescard__item">Basketball</li>
-                <li className="featurescard__item">Tennis</li>
-                <li className="featurescard__item">Pickleball</li>
-                <li className="featurescard__item">Video Games</li>
-                <li className="featurescard__item">Cubing</li>
-                <li className="featurescard__item">Coding</li>
-                <li className="featurescard__item">Baking</li>
-                <li className="featurescard__item">Video Editing</li>
-                <li className="featurescard__item">Vinyl</li>
+                <li className="hobbiescard__item">Pool (Snooker, 9/10 ball, Straight)</li>
+                <li className="hobbiescard__item">Manga</li>
+                <li className="hobbiescard__item">Movies</li>
+                <li className="hobbiescard__item">Soccer</li>
+                <li className="hobbiescard__item">Basketball</li>
+                <li className="hobbiescard__item">Tennis</li>
+                <li className="hobbiescard__item">Pickleball</li>
+                <li className="hobbiescard__item">Video Games</li>
+                <li className="hobbiescard__item">Cubing</li>
+                <li className="hobbiescard__item">Coding</li>
+                <li className="hobbiescard__item">Baking</li>
+                <li className="hobbiescard__item">Video Editing</li>
+                <li className="hobbiescard__item">Vinyl</li>
             </ul>
         </div>
     </div>
