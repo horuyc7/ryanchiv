2312b77e06d4269bf12429dcde2e1ceabbc0463e
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-06-24 15:55:14 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function handleDownloadPDF() {
   // Create a link element
   const link = document.createElement('a');
   // Set the href to the URL of your PDF file
-  link.href = 'PUBLIC_URL/RyanChivResume.pdf';
+  link.href = '${process.env.PUBLIC_URL}/RyanChivResume.pdf';
   // Set the download attribute to the desired file name
   link.download = 'RyanChivResume.pdf';
 
