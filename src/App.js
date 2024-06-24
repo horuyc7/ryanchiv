3226784f18d856994821c2a76c0c0167df0592d3
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2024-06-24 15:38:45 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, Navigate} from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Movies from "./component/Movies";
 import Manga from "./component/Manga";
-import Slide from "./component/Slide";
+import Travel from "./component/Travel";
 import Restaurants from './component/Restaurants';
 import SpotifyDashboard from './component/SpotifyDashboard';
 import Vinyl from './component/Vinyl';
@@ -15,6 +15,61 @@ import Hobbies from './component/Hobbies';
 import './App.css';
 import FeaturesCard from './component/FeaturesCard';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+import DescriptionIcon from '@mui/icons-material/Description';
+import { colors } from '@mui/material';
+import { Save } from '@mui/icons-material';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+
+const actions = [
+  { icon: <DescriptionIcon />, name: 'Resume', action: () => handleDownloadPDF()},
+  { icon: <ShareIcon />, name: 'Share' },
+  //{ icon: <PrintIcon />, name: 'Print' },
+ // { icon: <SaveIcon />, name: 'Save'},
+];
+
+function handleDownloadPDF() {
+  // Create a link element
+  const link = document.createElement('a');
+  // Set the href to the URL of your PDF file
+  link.href = '/src/data/RyanChivResume.pdf';
+  // Set the download attribute to the desired file name
+  link.download = 'RyanChivResume.pdf';
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#d6550e',
+      
+    },
+
+    secondary: {
+      main: '#8be4a9',
+      
+    },
+
+
+    mode: 'dark',
+
+    
+  },
+
+
+});
 
 
 const ExternalLink = ({ to, children }) => (
@@ -28,6 +83,66 @@ const ExternalLink = ({ to, children }) => (
 );
 
 
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+function CustomSpeedDial(props) {
+  const [scrollDirection, setScrollDirection] = useState('up');
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY) {
+        setScrollDirection('down');
+      } else {
+        setScrollDirection('up');
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <Slide direction="up" in={scrollDirection === 'up'}>
+      <SpeedDial
+        ariaLabel="SpeedDial example"
+        sx={{ position: 'fixed', bottom: 14, right: 14 }}
+        icon={<SpeedDialIcon />}
+        FabProps={{
+          sx: {
+            bgcolor: '#8be4a9',
+            '&:hover': {
+              bgcolor: '#79c992',
+            },
+          },
+        }}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={action.action} // Attach the action handler
+          />
+        ))}
+      </SpeedDial>
+    </Slide>
+  );
+}
+
 export default function App() {
   
   return (
@@ -36,6 +151,8 @@ export default function App() {
           <div className='drawer'>
                 <TemporaryDrawer /> {/* Render the TemporaryDrawer component */}
           </div>
+    
+          <CustomSpeedDial />
         
 
           <div className="container">
@@ -44,7 +161,7 @@ export default function App() {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/hobbies" element={<Hobbies />} />
                 <Route path="/features" element={<FeaturesCard />} />
-                <Route path="/travel" element={<Slide />} />
+                <Route path="/travel" element={<Travel />} />
                 <Route path="/movies" element={<Movies />} />
                 <Route path="/manga" element={<Manga />} />
                 <Route path="/spotify/*" element={<SpotifyDashboard />} />
