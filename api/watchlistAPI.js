57b700e8d1d7644d467f1c73f352f70ed94a738b
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2025-05-30 00:02:04 -0500

    Update watchlistAPI.js

--- api/watchlistAPI.js
+++ api/watchlistAPI.js
@@ -3,100 +3,89 @@ const cheerio = require('cheerio');
 
 module.exports = async (req, res) => {
     try {
-        // Get letterboxd link from Vercel env var
-        const responseList = await axios.get(process.env.LETTERBOXD_WATCHLIST);
-        const $list = cheerio.load(responseList.data);
+        const watchlistUrls = process.env.LETTERBOXD_WATCHLIST.split(',');
 
-        // Get list title and description from HTML
-        const listDetails = {
-            title: $list('.title-1').text().trim(),
-            description: $list('.body-text p').text().trim(),
-        };
+        const allWatchlistData = [];
 
-        const moviesData = [];
-        const posterPromises = [];
+        for (const url of watchlistUrls) {
+            const responseList = await axios.get(url);
+            const $list = cheerio.load(responseList.data);
 
-        // Get every movie from list
-        $list('.poster-container').each(async (index, element) => {
-            const href = $list(element).find('[data-target-link]').attr('data-target-link');
-            const posterUrl = `https://letterboxd.com/ajax/poster${href}std/1000x1300/`;
+            const listDetails = {
+                title: $list('.title-1').text().trim(),
+                description: $list('.body-text p').text().trim(),
+            };
 
-            posterPromises.push(
-                axios.get(posterUrl)
-                    .then(posterResponse => {
-                        const posterData = posterResponse.data;
-                        const imageUrl = cheerio.load(posterData)('img').attr('src') || cheerio.load(posterData)('img').attr('srcset');
-                        moviesData.push({ href, imageUrl });
-                    })
-                    .catch(error => {
-                        console.error('Error fetching poster:', error);
-                    })
-            );
-        });
+            const moviesData = [];
+            const posterPromises = [];
 
-        await Promise.all(posterPromises);
+            $list('.poster-container').each((index, element) => {
+                const href = $list(element).find('[data-target-link]').attr('data-target-link');
+                const posterUrl = `https://letterboxd.com/ajax/poster${href}std/1000x1300/`;
 
-        // Fetch additional details for each movie
-        const moviesDetailsPromises = moviesData.map(async movie => {
-            const responseMovie = await axios.get(`https://letterboxd.com${movie.href}`);
-            const $movie = cheerio.load(responseMovie.data);
-
-            const title = $movie('.headline-1').text().trim();
-            const tagline = $movie('.tagline').text().trim();
-            const description = $movie('.truncate p').text().trim();
-            const genres = [];
-            $movie('#tab-genres .text-sluglist a').each((index, element) => {
-                genres.push($movie(element).text());
+                posterPromises.push(
+                    axios.get(posterUrl)
+                        .then(posterResponse => {
+                            const $poster = cheerio.load(posterResponse.data);
+                            const imageUrl = $poster('img').attr('src') || $poster('img').attr('srcset');
+                            moviesData.push({ href, imageUrl });
+                        })
+                        .catch(error => {
+                            console.error('Error fetching poster:', error);
+                        })
+                );
             });
 
-            const responseRating = await axios.get(`https://letterboxd.com/csi${movie.href}rating-histogram`);
-            const $rating = cheerio.load(responseRating.data);
-            const rating = $rating('.average-rating a').text().trim();
+            await Promise.all(posterPromises);
 
+            const moviesDetailsPromises = moviesData.map(async movie => {
+                const responseMovie = await axios.get(`https://letterboxd.com${movie.href}`);
+                const $movie = cheerio.load(responseMovie.data);
 
-            const userReviews = [];
-            $movie('.film-detail').each((index, element) => {
-                if (index < 3) { // Only extract bodyText for the first two movies
-                    
-                    
-                    const $detail = $movie(element);
+                const title = $movie('.headline-1').text().trim();
+                const tagline = $movie('.tagline').text().trim();
+                const description = $movie('.truncate p').text().trim();
 
-                    const hiddenSpoilersExist = $detail.find('.hidden-spoilers').length > 0;
-                    
-                    
-                    
-                    let bodyText;
-                    if (hiddenSpoilersExist) {
-                        bodyText = $detail.find('.hidden-spoilers p').text().trim();
-                    } else {
-                        bodyText = $detail.find('.body-text p').text().trim();
+                const genres = [];
+                $movie('#tab-genres .text-sluglist a').each((i, el) => {
+                    genres.push($movie(el).text());
+                });
+
+                const responseRating = await axios.get(`https://letterboxd.com/csi${movie.href}rating-histogram`);
+                const $rating = cheerio.load(responseRating.data);
+                const rating = $rating('.average-rating a').text().trim();
+
+                const userReviews = [];
+                $movie('.film-detail').each((index, element) => {
+                    if (index < 3) {
+                        const $detail = $movie(element);
+                        const hiddenSpoilers = $detail.find('.hidden-spoilers');
+                        const bodyText = hiddenSpoilers.length > 0 ?
+                            hiddenSpoilers.find('p').text().trim() :
+                            $detail.find('.body-text p').text().trim();
+
+                        const name = $detail.find('.attribution-block .name').text().trim();
+                        const reviewRating = $detail.find('.rating.-green').text().trim();
+
+                        userReviews.push({ name, rating: reviewRating, text: bodyText });
                     }
+                });
 
-                    //const  bodyText = $detail.find('.body-text p').text().trim();
-                   
-                    const name = $detail.find('.attribution-block .name').text().trim();
-                    const rating = $detail.find('.rating.-green').text().trim();
-                    
-                    const review = {
-                        name: name,
-                        rating: rating,
-                        text: bodyText
-                    };
-                    
-                    
-                    userReviews.push(review);
-                }
+                return { title, rating, tagline, description, genres, userReviews };
             });
 
-            return { title, rating, tagline, description, genres, userReviews};
-        });
+            const moviesDetails = await Promise.all(moviesDetailsPromises);
 
-        const moviesDetails = await Promise.all(moviesDetailsPromises);
+            allWatchlistData.push({
+                listDetails,
+                movies: moviesData,
+                moviesDetails,
+            });
+        }
 
-
-        res.json({ moviesData, listDetails, moviesDetails });
+        res.json({ watchlists: allWatchlistData });
     } catch (error) {
         console.error('Error scraping movies:', error);
-        throw error;
+        res.status(500).json({ error: 'Failed to fetch watchlists' });
     }
-}
+};
