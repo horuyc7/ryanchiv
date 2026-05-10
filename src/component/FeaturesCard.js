2f32da854255df4fcd1e8cb1bd79d6a4c7941675
Ryan Chiv <69568816+horuyc7@users.noreply.github.com>
2026-05-10 22:08:51 +0700

    Update FeaturesCard.js

--- src/component/FeaturesCard.js
+++ src/component/FeaturesCard.js
@@ -9,27 +9,23 @@ export default function FeaturesCard() {
         <div className="featurescard__body">
             <ul className="featurescard_items">
 
-                <li className="featurescard__item">Restaurant/Cafe ratings with orders and places</li>
-                <li className="featurescard__item">Book collection</li>
+                <li className="featurescard__item">Books collection</li>
                 <li className="featurescard__item">Train model for object detection(more index)</li>
                 <li className="featurescard__item">Clean up code</li>
                 <li className="featurescard__item">Refactor</li>
-                <li className="featurescard__item">More comments</li>
                 <li className="featurescard__item">Make Spotify recommendation more specific</li>
-                <li className="featurescard__item">Favorite places</li>
 
-
-       
-                <li className="featurescard__item">Make object detection table checkable to show specific objects</li>
+                <li className="featurescard__item">F̶a̶v̶o̶r̶i̶t̶e̶ ̶r̶e̶s̶t̶a̶u̶r̶a̶n̶t̶s̶/̶c̶a̶f̶e̶s̶/̶b̶a̶k̶e̶r̶y̶</li>
+                <li className="featurescard__item">M̶o̶v̶e̶ ̶f̶r̶o̶m̶ ̶e̶m̶b̶e̶d̶d̶e̶d̶ ̶t̶o̶ ̶l̶o̶c̶a̶l̶ ̶g̶a̶l̶l̶e̶r̶y̶</li>
+                <li className="featurescard__item">M̶a̶k̶e̶ ̶o̶b̶j̶e̶c̶t̶ ̶d̶e̶t̶e̶c̶t̶i̶o̶n̶ ̶t̶a̶b̶l̶e̶ ̶c̶h̶e̶c̶k̶-̶a̶b̶l̶e̶ ̶t̶o̶ ̶s̶h̶o̶w̶ ̶s̶p̶e̶c̶i̶f̶i̶c̶ ̶o̶b̶j̶e̶c̶t̶s̶</li>
                 <li className="featurescard__item">A̶d̶d̶ ̶h̶o̶b̶b̶i̶e̶s̶</li>
                 <li className="featurescard__item">A̶d̶d̶ ̶t̶i̶m̶e̶l̶i̶n̶e̶</li>
                 <li className="featurescard__item">A̶d̶d̶ ̶o̶p̶t̶i̶o̶n̶s̶ ̶t̶o̶ ̶r̶e̶c̶o̶m̶m̶e̶n̶d̶a̶t̶i̶o̶n̶</li>
-                <li className="featurescard__item">M̶a̶k̶e̶ ̶m̶a̶n̶g̶a̶ ̶d̶y̶n̶a̶m̶i̶c̶,̶ ̶u̶s̶e̶ ̶m̶y̶a̶n̶i̶m̶e̶l̶i̶s̶t̶ ̶A̶P̶I̶</li>
+                {/*<li className="featurescard__item">M̶a̶k̶e̶ ̶m̶a̶n̶g̶a̶ ̶d̶y̶n̶a̶m̶i̶c̶,̶ ̶u̶s̶e̶ ̶m̶y̶a̶n̶i̶m̶e̶l̶i̶s̶t̶ ̶A̶P̶I̶</li> */}
                 <li className="featurescard__item">A̶d̶d̶ ̶s̶p̶o̶t̶i̶f̶y̶ ̶p̶l̶a̶y̶l̶i̶s̶t̶</li>
                 <li className="featurescard__item">A̶d̶d̶ ̶s̶p̶o̶t̶i̶f̶y̶ ̶r̶e̶c̶o̶m̶m̶e̶n̶d̶a̶t̶i̶o̶n̶</li>
-                <li className="featurescard__item">S̶e̶t̶ ̶u̶p̶ ̶s̶p̶o̶t̶i̶f̶y̶ ̶a̶u̶t̶h̶o̶r̶i̶z̶a̶t̶i̶o̶n̶</li>
-                <li className="featurescard__item" >S̶k̶i̶l̶l̶s̶</li>
-                <li className="featurescard__item">M̶a̶n̶g̶a̶ p̶l̶a̶n̶ t̶o̶ r̶e̶a̶d̶</li>
+                <li className="featurescard__item">S̶k̶i̶l̶l̶s̶</li>
+                {/*<li className="featurescard__item">M̶a̶n̶g̶a̶ p̶l̶a̶n̶ t̶o̶ r̶e̶a̶d̶</li>*/}
                 <li className="featurescard__item">C̶u̶s̶t̶o̶m̶ ̶f̶i̶l̶t̶e̶r̶ ̶f̶o̶r̶ ̶s̶p̶o̶t̶i̶f̶y̶</li>
                 
             </ul>
